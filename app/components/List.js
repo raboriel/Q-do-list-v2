8e1dd270d5e55246b6cb0ce6377d4f6098a5d897
Kyungsub Kim <raboriel@gmail.com>
2019-06-14 15:33:20 -0400

    add condition for toggle

--- app/components/List.js
+++ app/components/List.js
@@ -7,10 +7,10 @@ const { height, width } = Dimensions.get('window');
 class List extends Component {
   // toggle for checkbox
   onToggleCircle = () => {
-    const { isCompleted, id, completeItem, incompleteList } = this.props;
+    const { isCompleted, id, completeList, incompleteList } = this.props;
     console.log(id);
     if (isCompleted) {
-      incompleteItem(id);
+      incompleteList(id);
     } else {
       completeItme(id);
     }
@@ -19,8 +19,43 @@ class List extends Component {
     const { text, deleteList, id, isCompleted }
     return (
       <View style={styles.container}>
-      </View>
-
+             <View style={styles.column}>
+               <TouchableOpacity onPress={this.onToggleCircle}>
+                 <View
+                   style={[
+                     styles.circle,
+                     isCompleted
+                       ? { borderColor: '#90ee90' }
+                       : { borderColor: '#ecbfbe' }
+                   ]}
+                 />
+               </TouchableOpacity>
+               <Text
+                 style={[
+                   styles.text,
+                   isCompleted
+                     ? {
+                         color: '#c4c4cc',
+                         textDecorationLine: 'line-through'
+                       }
+                     : { color: '#555555' }
+                 ]}
+               >
+                 {text}
+               </Text>
+             </View>
+             {isCompleted ? (
+               <View style={styles.button}>
+                 <TouchableOpacity onPressOut={() => deleteList(id)}>
+                   <MaterialIcons
+                     name="delete-forever"
+                     size={24}
+                     color={deleteIconColor}
+                   />
+                 </TouchableOpacity>
+               </View>
+             ) : null}
+           </View>
     );
   }
 }
@@ -28,7 +63,14 @@ class List extends Component {
 const styles = StyleSheet.create({
   container: {
   },
-
+  column: {
+  },
+  text: {
+  },
+  circle: {
+  },
+  button: {
+  }
 });
 
 
